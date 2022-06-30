c56d38eb708dd9e84aae5b25ce438cde37d49610
홍순원 <hongsun-won@hongsun-won-ui-MacBookAir.local>
2022-06-30 23:21:50 +0900

    unit test 1

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -1,4 +1,6 @@
+import { NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
+import { Movie } from './entities/movie.entity';
 import { MoviesService } from './movies.service';
 
 describe('MoviesService', () => {
@@ -12,7 +14,48 @@ describe('MoviesService', () => {
     service = module.get<MoviesService>(MoviesService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('getAll', () => {
+    it('should be return an array', () => {
+      const result = service.getAll();
+      expect(result).toBeInstanceOf(Array);
+    });
   });
+
+  describe('getOne', () => {
+    it('should be return a movie', () => {
+      service.createOne({
+        title: 'Test Movie',
+        genres: ['test genres'],
+        year: 2022,
+      });
+      const result = service.getOne(1);
+  
+      expect(result).toBeDefined();
+      expect(result.id).toEqual(1);
+    });
+  
+    it('should throw 404 error', () => {
+      try{
+       const result = service.getOne(999);
+      }catch(e){
+        expect(e).toBeInstanceOf(NotFoundException);
+        expect(e.message).toEqual(`Movie with ID: 999 not found.`);
+      }
+    });
+    
+  });
+
+  // describe('deleteOne', () => {
+  //   it('should be return an array', () => {
+  //     service.createOne({
+  //       title: 'Test Movie',
+  //       genres: ['test genres'],
+  //       year: 2022,
+  //     });
+
+  //     const result = service.getOne(1);
+
+  //     expect(result).toBeInstanceOf(Array);
+  //   });
+  // });
 });
