86fff10f2d8442c2e70e2020f54ac136fdec2779
홍순원 <hongsun-won@hongsun-won-ui-MacBookAir.local>
2022-06-30 22:21:11 +0900

    validation

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post, Put, Query } from '@nestjs/common';
+import { CreateMovieDto } from './dto/create-movie.dto';
 import { Movie } from './entities/movie.entity';
 import { MoviesService } from './movies.service';
 
@@ -18,25 +19,22 @@ export class MoviesController {
     // }
 
     @Get(":id")
-    getOne(@Param('id') movieId:string): Movie{
+    getOne(@Param('id') movieId:number): Movie{
         return this.moviesService.getOne(movieId);
     }
 
     @Post()
-    create(@Body() movieData){
+    create(@Body() movieData: CreateMovieDto){
         return this.moviesService.createOne(movieData);
     }
 
     @Delete(':id')
-    remove(@Param('id') movieId:string) {
+    remove(@Param('id') movieId:number) {
         return this.moviesService.deleteOne(movieId);
     }
 
     @Patch(':id')
-    patch(@Param('id') movieId: string, @Body() updateData): string{
-        return {
-            updateMovie: movieId,
-            ...updateData,
-        };
+    patch(@Param('id') movieId: number, @Body() updateData){
+        return this.moviesService.update(movieId, updateData);
     }
 }
